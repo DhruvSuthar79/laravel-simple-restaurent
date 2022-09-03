997ea737c5749c744f2c3ee298d31b2d30b34e19
DhruvSuthar79 <60097511+DhruvSuthar79@users.noreply.github.com>
2022-09-03 22:54:04 +0530

    Update about.blade.php

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -21,7 +21,7 @@
           <div class="detail-box">
             <div class="heading_container">
               <h2>
-                We Are Feane Ok
+                We Are Feane
               </h2>
             </div>
             <p>
